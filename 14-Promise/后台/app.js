91a98a11a05e561d4535a714141ab3d7c272062e
sunyesheng <1481633100@qq.com>
2021-06-17 15:22:13 +0800

    999999999999999

--- 14-Promise/后台/app.js
+++ 14-Promise/后台/app.js
@@ -11,8 +11,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Methods", "PUT,GET,POST,DELETE,OPTIONS")
-    res.header("Access-Control-Allow-Headers", "X-Requested-With")
-    res.header("Accexx-Control-Allow-Headers", "Content-Type")
+    //res.header("Access-Control-Allow-Headers",)
+    res.header("Access-Control-Allow-Headers", ["X-Requested-With", "Content-Type", "mytoken"])
     next()
 })
 
